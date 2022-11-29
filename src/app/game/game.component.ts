a909bb095c227fe2eb4eb182f903180581257ee4
S00211744 <S00211744@atu.ie>
2022-11-29 20:41:13 +0000

    Fixed some errors, Need to do CORS Policy on API

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -8,16 +8,16 @@ import { GameService } from '../game.service';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-  games: Game[] = [];
+  gamesList: Game[] = [];
   aError: String = '';
 
   constructor(private gameService: GameService) { }
 
   ngOnInit(): void {
     this.gameService.getGames().subscribe({
-      next: (value: Game[]) => this.games = value,
+      next: (value: Game[]) => this.gamesList = value,
       complete: () => console.log("Games listed"),
-      error: (aError) => this.aError = this.aError,
+      error: (aError) => this.aError = this.aError
     })
   }
 
