aa31e801cd2333906bbd130d780f4c00829a71c1
s00212257 <s00212257@atu.ie>
2022-11-29 12:23:11 +0000

    Game service and component created

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  //TODO: Host the API somewhere and point this to it
+  apiUri: 'http://localhost:3000'
 };
 
 /*
