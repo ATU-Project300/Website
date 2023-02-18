6be797a584affd6d3ee367cb1e6ff3b935e725e7
DawidKopecki <s00212387@atu.ie>
2023-02-18 01:50:21 +0000

    current progress
    implement a dark theme mode and added button that scrolls to top of the page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,34 @@ import { ViewEncapsulation } from '@angular/core';
 })
 export class AppComponent {
   title = 'website';
+  isShow!: boolean;
+  topPosToStartShowing = 100;
+  toggleDarkTheme(): void {
+    document.body.classList.toggle('dark-theme');
+  }
+  @HostListener('window:scroll')
+  checkScroll() {
+      
+    // window의 scroll top
+    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+    
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  // TODO: Cross browsing
+  gotoTop() {
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
+  }
 }
