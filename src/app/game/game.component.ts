96284c0daca7cb6d9174c08dc3487692b6452cfb
DawidKopecki <s00212387@atu.ie>
2023-03-01 01:49:52 +0000

    added search/filtering
    added search and filtering user can use search bar to filter through games by their title, year, console, emulator.

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Game } from '../game';
 import { GameService } from '../game.service';
 
@@ -7,13 +7,12 @@ import { GameService } from '../game.service';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
-export class GameComponent implements OnInit{
+export class GameComponent implements OnInit, AfterViewInit {
   gamesList: Game[] = [];
   aError: String = '';
-  elementRef: any;
+  @ViewChild('searchInput') searchInput!: ElementRef;
 
   constructor(private gameService: GameService) { }
-
 
   ngOnInit(): void {
     this.gameService.getGames().subscribe({
@@ -21,6 +20,30 @@ export class GameComponent implements OnInit{
       complete: () => console.log("Games listed"),
       error: (aError) => this.aError = this.aError
     })
+  }
+
+  ngAfterViewInit(): void {
+    // focus on the search input element after the view has initialized
+    this.searchInput.nativeElement.focus();
+  }
+
+  searchGames(searchTerm: string): void {
+    // filter the games list based on the search term, year, console, and emulator
+    this.gamesList = this.gamesList.filter(game =>
+      game.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      game.year.toString().includes(searchTerm) ||
+      game.consoles.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      game.emulator.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+
+  resetGames(): void {
+    // reset the games list to its original state
+    this.gameService.getGames().subscribe({
+      next: (value: Game[]) => this.gamesList = value,
+      complete: () => console.log("Games listed"),
+      error: (aError) => this.aError = this.aError
+    });
   }
 
 }
