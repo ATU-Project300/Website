74b35ff3b99c537493e201452d3ad6c15cc55fa7
DawidKopecki <s00212387@atu.ie>
2023-03-19 01:56:11 +0000

    updated add game form
    added preview for image and added option to upload image using The previewImage method is for previewing an image that is selected from the user's device and converting it into a URL using the FileReader API. This is typically used when the user wants to see a preview of the image before uploading it to a server.

--- src/app/components/add-game/add-game.component.ts
+++ src/app/components/add-game/add-game.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { Game } from '../../models/game';
 
 @Component({
@@ -11,33 +11,72 @@ export class AddGameComponent implements OnInit {
 
   @Output() gameFormClose = new EventEmitter();
   @Input() game?: Game;
-  gameForm : FormGroup = new FormGroup({});
+  gameForm: FormGroup = new FormGroup({});
+  previewImageUrl: string | ArrayBuffer | null = null;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.gameForm  = new FormGroup({
-      title: new FormControl (this.gameForm?.get('title'), [Validators.required, Validators.minLength(4)]),
-      year: new FormControl (this.gameForm?.get('year'), [Validators.required, Validators.min(1980), Validators.max(2023)]),
-      description: new FormControl(this.gameForm?.get('description'), [Validators.required, Validators.minLength(10)]),
-      image: new FormControl(this.gameForm?.get('image'), [Validators.required, Validators.minLength(5)]),
-      consoles: new FormControl(this.gameForm?.get('consoles'), [Validators.required, Validators.minLength(3)]),
-      emulator: new FormControl(this.gameForm?.get('emulator'), [Validators.required, Validators.minLength(3)]),
-    })
+    this.gameForm = new FormGroup({
+      title: new FormControl(this.game?.title, [Validators.required, Validators.minLength(4)]),
+      year: new FormControl(this.game?.year, [Validators.required, Validators.min(1980), Validators.max(2023)]),
+      description: new FormControl(this.game?.description, [Validators.required, Validators.minLength(10)]),
+      image: new FormControl(null),
+      imageUrl: new FormControl(null),
+      consoles: new FormControl(this.game?.consoles, [Validators.required, Validators.minLength(3)]),
+      emulator: new FormControl(this.game?.emulator, [Validators.required, Validators.minLength(3)]),
+    }, { validators: [this.imageRequiredValidator] });
+    this.previewImageUrl = this.game?.image || null;
   }
 
   onSubmit() {
-    console.log('forms submitted with: ');
-    console.table(this.gameForm?.value);
-    this.gameFormClose.emit(this.gameForm?.value);
+    console.log('Form submitted with:');
+    console.table(this.gameForm.value);
+    this.gameFormClose.emit(this.gameForm.value);
   }
 
   closeForm() {
-    this.gameFormClose.emit(undefined)
+    this.gameFormClose.emit(undefined);
   }
 
   get title() {
-    return this.gameForm?.get('title');
+    return this.gameForm.get('title');
   }
 
+  previewImage(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.previewImageUrl = reader.result;
+      };
+    }
+  }
+
+  imageRequiredValidator(control: AbstractControl): ValidationErrors | null {
+    const imageControl = control.get('image');
+    const imageUrlControl = control.get('imageUrl');
+    const image = imageControl?.value;
+    const imageUrl = imageUrlControl?.value;
+  
+    if (!image && !imageUrl) {
+      return { imageRequired: true };
+    } else if (image && imageUrl) {
+      imageUrlControl?.setErrors({ cannotBeUsedWithImage: true });
+      return { cannotBeUsedWithImage: true };
+    } else if (image) {
+      const reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onload = () => {
+        if (imageUrlControl) {
+          imageUrlControl.setValue(reader.result);
+        }
+      };
+    } else if (imageUrl) {
+      this.previewImageUrl = imageUrl;
+    }
+  
+    return null;
+  }
 }
